9ebc0f280850710b95d5966141e66e55b54f7e98
simonepadoan (Dr ) <simone.padoan@unibocconi.it>
2010-11-08 18:52:15 +0000

    Commited Gradient.c
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/comprandfld@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/Gradient.c
@@ -0,0 +1,13 @@
+#include "header.h"
+
+/*###################################################
+### Authors: Simone Padoan and Moreno Bevilacqua.
+### Email: simone.padoan@epfl.ch.
+### Institute: EPFL.
+### File name: Gradient.c
+### Description:
+### This file contains a set of procedures
+### for the computation of the composite likelihood
+### gradients.
+### Last change: 07/11/2010.
+##################################################*/
